7e8201b85229e3bbe40c911ac32fd08d0290c0c1
Nikita_Seropyan <NGSeropyan@yandex.ru>
2024-07-07 19:06:12 +0300

    Create tests for all modules and add result of tests in file README.md

--- src/widget.py
+++ src/widget.py
@@ -6,14 +6,24 @@ def mask_account_card(my_string: str) -> str:
     product_name = [i for i in my_string.split() if i.isalpha()]
     product_number = [i for i in my_string.split() if i.isdigit()]
     mask = None
-    if len(product_number[0]) > 16:
-        mask = get_mask_account(product_number[0])
-    else:
-        mask = get_mask_card_number(product_number[0])
-    return " ".join(product_name) + " " + mask
+    if len(product_number) > 0:
+        if len(product_number[0]) > 16:
+            mask = get_mask_account(product_number[0])
+        else:
+            mask = get_mask_card_number(product_number[0])
+        return " ".join(product_name) + " " + mask
+    raise ValueError("Номер отсутствует!")
 
 
 def get_data(date_string: str) -> str:
     """Функция возвращающая дату в корректном формате"""
-    get_date = date_string[:10].split("-")
-    return ".".join(get_date[::-1])
+    if len(date_string) >= 26 and "-" in date_string:
+        get_date = date_string[:10].split("-")
+        flag = True
+        for digit in get_date:
+            for char in digit:
+                if char.isalpha():
+                    flag = False
+        if flag:
+            return ".".join(get_date[::-1])
+    raise ValueError("Неверный формат даты!")
